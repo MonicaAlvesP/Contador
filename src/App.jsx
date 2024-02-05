fac08d4c4708e0be93f33b46ae8ac4e486242299
MonicaAlvesP <monica52782@gmail.com>
2024-02-05 19:39:46 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react' // Importando React
+import Contador from './Contador/Contador.jsx'
+import './App.css'
+
+function App() {
+
+
+  return(
+    <>
+    <h1>Olá T3, SEGUNDOU!!!</h1>
+    <Contador />
+    </>
+    
+  )
+}
+
+export default App
